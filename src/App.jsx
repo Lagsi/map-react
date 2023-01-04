e217f18308378794107d98b48ce10aa6c4f9a543
Jeppe Hauman <jeppe_hauman@hotmail.com>
2023-01-04 11:31:24 +0100

    guardian api start

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,38 @@ import "react-svg-map/lib/index.css";
 import { getLocationId, getLocationName } from "./utils";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const mapRef = useRef();
-  useEffect(() => {
-    console.log(mapRef.current);
-  }, []);
+  const [data, setData] = useState();
+  const [link, setLink] = useState();
+  let header;
 
   function onClick(event) {
-    console.log(getLocationName(event));
+    const country = getLocationName(event);
+
+    try {
+      fetch(
+        `https://content.guardianapis.com/search?order-by=newest&q=${country}&api-key=${
+          import.meta.env.VITE_GUARDIAN_API
+        }`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.response.results);
+          setData(data.response.results);
+          setLink(data.response.results[0].webUrl);
+        });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
     <div className="map-container">
       <SVGMap map={World} onLocationClick={onClick} />
+      {
+        <h1>
+          <a href={link}>{data}</a>
+        </h1>
+      }
     </div>
   );
 }
