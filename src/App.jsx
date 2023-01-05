ac74d30f0a74dcd3d440ea864f1dca5555150c3c
Nima <nima97e@gmail.com>
2023-01-05 18:04:36 +0330

    change some things

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { useState } from "react";
-import "./App.css";
-import World from "@svg-maps/world";
 import { SVGMap } from "react-svg-map";
-import "react-svg-map/lib/index.css";
 import { getLocationName } from "./utils";
+import World from "@svg-maps/world";
 import Articles from './components/Articles'
+import "react-svg-map/lib/index.css";
+import "./App.css";
+
 function App() {
   const [data, setData] = useState([]);
 
-
-  function onClick(event) {
+  const onClick = (event) => {
     const country = getLocationName(event);
-
     try {
       fetch(
         `https://content.guardianapis.com/search?show-tags=contributor&show-references=author&q=${country}&show-fields=thumbnail&api-key=${import.meta.env.VITE_GUARDIAN_API
@@ -19,16 +18,12 @@ function App() {
       )
         .then((response) => response.json())
         .then((data) => {
-          console.log(data.response.results);
-          let newArr = [];
-          newArr = data.response.results;
-          setData(newArr);
+          setData(data.response.results);
         });
     } catch (error) {
       console.log(error);
     }
   }
-
   return (
     <>
       <div className="map-container">
