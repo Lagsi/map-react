6683ae7525f19239e0cb9fecf61f7ebddaf1da4b
Jeppe Hauman <jeppe_hauman@hotmail.com>
2023-01-09 14:07:51 +0100

    Add hover text to show country. Scroll to articles when loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { SVGMap } from "react-svg-map";
 import { getLocationName } from "./utils";
 import World from "@svg-maps/world";
 import Articles from "./components/Articles";
 import "react-svg-map/lib/index.css";
 import "./App.css";
+import HoverDisplay from "./components/hoverDisplay";
+
+// function hoverDisplay({ country }) {
+//   return (
+//     <div>
+//       <span>{country}</span>
+//     </div>
+//   );
+// }
 
 function App() {
   const [data, setData] = useState([]);
+  const [country, setCountry] = useState("Hover a country");
+  const myElement = useRef(null);
+
+  function mouseOver(event) {
+    setCountry(getLocationName(event));
+  }
+
+  const scrollToNews = () => {
+    // myElement.current.scrollIntoView({ behavior: "smooth", block: "end" });
+    window.scrollTo({
+      top: 500,
+      behavior: "smooth",
+    });
+  };
 
   const onClick = (event) => {
     const country = getLocationName(event);
@@ -17,7 +40,7 @@ function App() {
         `,
         {
           headers: {
-            "x-api-key": import.meta.env.VITE_NEWSCATCHER_KEY2,
+            "x-api-key": import.meta.env.VITE_NEWSCATCHER_KEY,
           },
         }
       )
@@ -31,7 +54,7 @@ function App() {
               `,
                   {
                     headers: {
-                      "x-api-key": import.meta.env.VITE_NEWSCATCHER_KEY2,
+                      "x-api-key": import.meta.env.VITE_NEWSCATCHER_KEY,
                     },
                   }
                 )
@@ -47,14 +70,15 @@ function App() {
                       return false;
                     });
                     setData(filteredArr);
-                    console.log(filteredArr);
+                    scrollToNews();
                   });
               } catch (error) {
                 console.log(error);
               }
             }, 1200);
           }
-          if (data.articles > 4) setData(data.articles);
+          if (data.articles.length > 4) setData(data.articles);
+          scrollToNews();
         });
     } catch (error) {
       console.log(error);
@@ -62,8 +86,13 @@ function App() {
   };
   return (
     <>
-      <div className="map-container">
-        <SVGMap map={World} onLocationClick={onClick} />
+      <HoverDisplay country={country} />
+      <div ref={myElement} className="map-container">
+        <SVGMap
+          map={World}
+          onLocationClick={onClick}
+          onLocationMouseOver={mouseOver}
+        />
       </div>
       <div className="articles-container">
         <Articles data={data} />
---
+++ src/components/hoverDisplay.jsx
@@ -0,0 +1,9 @@
+function HoverDisplay({ country }) {
+  return (
+    <div className="hover-display">
+      <span className="hover-country">{country}</span>
+    </div>
+  );
+}
+
+export default HoverDisplay;
