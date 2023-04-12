b4ab8c760338dd202445f74a778946eb820b69aa
Nima <nima97e@gmail.com>
2023-04-12 17:37:43 +0330

    remove redundant code

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,16 @@ import { SVGMap, Map, RadioSVGMap } from "react-svg-map";
 import { getLocationName, getLocationSelected } from "./utils";
 import W from "@svg-maps/world";
 const World = W.default ? W.default : W;
-import Modal from "./components/Modal";
 
-// import S from "react-switch";
-// const Switch = S.default? S.default: S
+
 import Articles from "./components/Articles";
 import "react-svg-map/lib/index.css";
 import "./App.css";
-import HoverDisplay from "./components/hoverDisplay";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Header from "./components/Header";
-import localData from "../data.json";
+
 import Saved from "./components/saved";
 import { Countries } from "../countries.js";
 import TInput from "react-autocomplete-input";
@@ -23,7 +21,6 @@ import "react-autocomplete-input/dist/bundle.css";
 
 function App() {
   const [data, setData] = useState([]);
-  const [country, setCountry] = useState("Hover a country");
   const [clickedCountry, setClickedCountry] = useState(null);
   const myElement = useRef(null);
   const [inputValue, setInputValue] = useState("");
@@ -83,71 +80,6 @@ function App() {
       console.log(error);
     }
 
-    //   try {
-    //     fetch(
-    //       `https://api.newscatcherapi.com/v2/search?q=${country}&lang=en,fa&search_in=title&sort_by=date&sort_by=rank&sources=ap.org,reuters.com,cnn.com,bbc.com,iranintl.com,bbc.co.uk,nytimes.com,theguardian.com,
-    //       `,
-    //       {
-    //         headers: {
-    //           "x-api-key": import.meta.env.VITE_NEWSCATCHER_KEY,
-    //         },
-    //       }
-    //     )
-    //       .then((response) => response.json())
-    //       .then((data) => {
-    //         console.log(data.articles);
-    //         if (!data.articles || data.articles.length < 5) {
-    //           setTimeout(() => {
-    //             try {
-    //               fetch(
-    //                 `https://api.newscatcherapi.com/v2/search?q=${country}&lang=en,fa&search_in=title&sort_by=date&to_rank=2000&not_sources=cts.businesswire.com,intelligenceonline.com,coinupdate.com,
-    //               `,
-    //                 {
-    //                   headers: {
-    //                     "x-api-key": import.meta.env.VITE_NEWSCATCHER_KEY,
-    //                   },
-    //                 }
-    //               )
-    //                 .then((response) => response.json())
-    //                 .then((dataWithoutAllSrcs) => {
-    //                   let arr = dataWithoutAllSrcs.articles;
-    //                   const uniqueTitles = new Set();
-    //                   const filteredArr = arr.filter((obj) => {
-    //                     if (!uniqueTitles.has(obj.title)) {
-    //                       uniqueTitles.add(obj.title);
-    //                       return true;
-    //                     }
-    //                     return false;
-    //                   });
-    //                   // filteredArr.articles.forEach((article) => {
-    //                   //   if (article.title.length > 200) {
-    //                   //     article.title = article.title.slice(0, 200);
-    //                   //   }
-    //                   // });
-    //                   setData(filteredArr);
-    //                   setLoading(false);
-    //                   scrollToNews();
-    //                   console.log(filteredArr);
-    //                 });
-    //             } catch (error) {
-    //               console.log(error);
-    //             }
-    //           }, 1200);
-    //           return;
-    //         }
-    //         data.articles.forEach((article) => {
-    //           if (article.title.length > 200) {
-    //             article.title = article.title.slice(0, 200);
-    //           }
-    //         });
-    //         if (data.articles.length > 4) setData(data.articles);
-    //         setLoading(false);
-    //         scrollToNews();
-    //       });
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    // };
   };
   const scrollToNews = () => {
     window.scrollTo({
@@ -163,8 +95,7 @@ function App() {
     const country = getLocationName(event);
     setClickedCountry(country);
     fetchApiData(country);
-    // console.log(getLocationSelected(event));
-    // setData(localData.articles);
+    
   };
   const handleChange = (event) => {
     setInputValue((prev) => (prev = event));
@@ -174,7 +105,6 @@ function App() {
     setClickedCountry(country);
     setInputValue("");
     fetchApiData(country);
-    //setData(localData.articles);
   };
   return (
     <>
@@ -221,16 +151,14 @@ function App() {
                     </div>
                   )}
 
-                  {/* <HoverDisplay country={country} /> */}
+                  
                   <div ref={myElement} className="map-container">
                     <SVGMap
                       map={World}
                       onLocationMouseOut={mouseOut}
                       onLocationClick={onClick}
                       onLocationMouseOver={mouseOver}
-                    // isLocationSelected={(location, index) => {
-                    //   console.log(index);
-                    // }}
+                    
                     />
                   </div>
                   <h2 className="clicked-country">{clickedCountry}</h2>
